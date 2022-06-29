ebbfc8ae2cc41d6f34d807ac8d4660aeab66cf64
Dan K <dankovacek@gmail.com>
2022-06-29 13:34:32 -0700

    fix paths

--- setup_scripts/MC_characterization.py
+++ setup_scripts/MC_characterization.py
@@ -29,6 +29,7 @@ wbt.verbose = False
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print(BASE_DIR)
 print(asdfsd)
 
 DATA_DIR = os.path.join(BASE_DIR, 'processed_data/')
